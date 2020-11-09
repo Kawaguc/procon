edbfca9fcfbb9f739ad59fca8825858fc57c35ba
Kawaguc <base1611ball@gmail.com>
2020-11-09 18:24:38 +0900

    add UnionFind.test.py for verification-helper

---
+++ UnionFind.test.py
@@ -0,0 +1,24 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+import sys
+
+from UnionFind import UnionFind
+
+input = sys.stdin.buffer.readline
+
+
+def main():
+    n, q = map(int, input().split())
+    uf = UnionFind(n)
+    for _ in range(q):
+        t, u, v = map(int, input().split())
+        if t == 0:
+            uf.unite(u, v)
+        elif t == 1:
+            if uf.same(u, v):
+                print(1)
+            else:
+                print(0)
+
+
+if __name__ == "__main__":
+    main()
