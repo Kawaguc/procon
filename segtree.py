c67013e952e93f1a9c1873349f96ea559beb87ff
Kawaguc <base1611ball@gmail.com>
2020-11-15 16:59:31 +0900

    added segtree.py and its oj-test(RMQ)

---
+++ segtree.py
@@ -0,0 +1,64 @@
+class SegmentTree:
+    def __init__(self, arr, operator, ide_ele):
+        """
+        arr: 元の配列
+        operator: 関数
+        ide_ele: 単位元
+        """
+        n = len(arr)
+        num = 1 << (n - 1).bit_length()  # n以上の最小の2べき
+        # bit_lengthの繰り上がりと最小の2べきの更新が1ずれている
+        self.offset = num  # 元の0-indexedの配列とセグ木の配列(1-indexed)の葉のインデックスの差
+        self.ide_ele = ide_ele
+        self.operator = operator
+        self.tree = [ide_ele] * (self.offset << 1)
+        for i in range(n):
+            self.tree[i + self.offset] = arr[i]
+        # 構築
+        for i in range(self.offset - 1, 0, -1):
+            self.tree[i] = self.operator(self.tree[2 * i], self.tree[2 * i + 1])
+
+    def update(self, k, x):
+        """
+        元の配列のindexがkの値をxに更新
+        """
+        k += self.offset
+        self.tree[k] = x
+        while k > 1:  # 親が存在するなら
+            self.tree[k >> 1] = self.operator(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
+
+    def query(self, l, r):
+        """
+        半開区間[l,r)のoperatorによる計算結果を得る
+        """
+        l += self.offset
+        r += self.offset
+        ret = self.ide_ele
+        while l < r:
+            if l & 1:
+                ret = self.operator(ret, self.tree[l])
+                l += 1
+            if r & 1:
+                ret = self.operator(ret, self.tree[r - 1])
+            l >>= 1  # 親の階層へ移動
+            r >>= 1
+        return ret
+
+
+def main():
+    n, q = map(int, input().split())
+    a = [2 ** 31 - 1] * n
+    ide_ele = 2 ** 31 - 1
+
+    seg = SegmentTree(a, min, ide_ele)
+    for _ in range(q):
+        c, x, y = map(int, input().split())
+        if c == 0:
+            seg.update(x, y)
+        if c == 1:
+            print(seg.query(x, y + 1))
+
+
+if __name__ == "__main__":
+    main()
---
+++ segtree.test.py
@@ -0,0 +1,20 @@
+# verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A
+from segtree import SegmentTree
+
+
+def main():
+    n, q = map(int, input().split())
+    a = [2 ** 31 - 1] * n
+    ide_ele = 2 ** 31 - 1
+
+    seg = SegmentTree(a, min, ide_ele)
+    for _ in range(q):
+        c, x, y = map(int, input().split())
+        if c == 0:
+            seg.update(x, y)
+        if c == 1:
+            print(seg.query(x, y + 1))
+
+
+if __name__ == "__main__":
+    main()
