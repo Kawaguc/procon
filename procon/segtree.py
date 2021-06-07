b69cfb62b0a14f132172f87126d0d7c992c5ddc5
kawagh <base1611ball@gmail.com>
2021-06-08 08:13:02 +0900

    modified segtree.py typehint,交換則を前提にしない,get method

--- procon/segtree.py
+++ procon/segtree.py
@@ -1,5 +1,8 @@
+from typing import Callable
+
+
 class SegmentTree:
-    def __init__(self, arr, operator, ide_ele):
+    def __init__(self, arr, operator: Callable, ide_ele):
         """
         arr: 元の配列
         operator: 関数
@@ -16,8 +19,7 @@ class SegmentTree:
             self.tree[i + self.offset] = arr[i]
         # 構築
         for i in range(self.offset - 1, 0, -1):
-            self.tree[i] = self.operator(
-                self.tree[2 * i], self.tree[2 * i + 1])
+            self.tree[i] = self.operator(self.tree[2 * i], self.tree[2 * i + 1])
 
     def update(self, k, x):
         """
@@ -26,8 +28,15 @@ class SegmentTree:
         k += self.offset
         self.tree[k] = x
         while k > 1:  # 親が存在するなら
-            self.tree[k >> 1] = self.operator(self.tree[k], self.tree[k ^ 1])
+            # 偶数indexが左
+            if k % 2 == 1:
+                self.tree[k >> 1] = self.operator(self.tree[k ^ 1], self.tree[k])
+            else:
+                self.tree[k >> 1] = self.operator(self.tree[k], self.tree[k ^ 1])
             k >>= 1
+
+    def get(self, i):
+        return self.tree[i + self.offset]
 
     def query(self, l, r):
         """
@@ -35,16 +44,17 @@ class SegmentTree:
         """
         l += self.offset
         r += self.offset
-        ret = self.ide_ele
+        lret = self.ide_ele
+        rret = self.ide_ele
         while l < r:
             if l & 1:
-                ret = self.operator(ret, self.tree[l])
+                lret = self.operator(lret, self.tree[l])
                 l += 1
             if r & 1:
-                ret = self.operator(ret, self.tree[r - 1])
+                rret = self.operator(self.tree[r - 1], rret)
             l >>= 1  # 親の階層へ移動
             r >>= 1
-        return ret
+        return self.operator(lret, rret)
 
 
 def main():
